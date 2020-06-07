17df4612da9db22c4a79d21c5a5eb25cf7244afb
Wawrzkowicz, Rafal <ext-rafal.wawrzkowicz@f-secure.com>
2020-06-07 17:53:36 +0200

    ClassNotFoundDemo

---
+++ src/main/java/ClassNotFoundDemo.java
@@ -0,0 +1,30 @@
+import jdk.internal.loader.ClassLoaders;
+
+import java.util.Arrays;
+
+public class ClassNotFoundDemo {
+
+    public static void main(String[] args) {
+        String fullQualifiedClassName = "I'm not here";
+
+        loadClass(ClassLoader.getSystemClassLoader(), fullQualifiedClassName);
+        loadClass(ClassLoader.getPlatformClassLoader(), fullQualifiedClassName);
+        loadClass(ClassLoaders.appClassLoader(), fullQualifiedClassName);
+        loadClass(ClassLoaders.platformClassLoader(), fullQualifiedClassName);
+    }
+
+    private static void loadClass(ClassLoader classLoader, String fullQualifiedClassName) {
+        try {
+            System.out.println("name: " + classLoader.getName());
+            System.out.println("parent: " + classLoader.getParent());
+            System.out.println("defined packages: " + Arrays.toString(classLoader.getDefinedPackages()));
+            System.out.println("isRegisteredAsParallelCapable:" + classLoader.isRegisteredAsParallelCapable());
+            System.out.println("classLoader loaded classLoader: " + classLoader.getClass().getClassLoader());
+
+            classLoader.loadClass(fullQualifiedClassName);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
